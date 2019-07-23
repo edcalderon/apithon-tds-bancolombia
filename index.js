592f26d588e0d67aa9268694c121c42eb6cd6130
edcalderon <edwardca12@gmail.com>
2019-07-23 17:35:08 -0500

    cors heders

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ contract.Hashlength((err,res) => {
 express()
 .use(bodyParser.json())
 .use(express.static(path.join(__dirname, 'public')))
+.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 .get('/',  (req, res) => {
     res.render('index', {});
 })
