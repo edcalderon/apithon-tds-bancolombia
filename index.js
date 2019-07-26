c8ea713e9b55df5c0b24e4dbb4ee364ddc5b9b8f
ed <edwardca12@gmail.com>
2019-07-26 18:05:07 -0400

    get last by pk

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ if (typeof web3 !== 'undefined') {
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-	web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/142f507682594dba987b1d47b2b175e4"));
+	web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/162c6026989446e08fb54b3fe3888f12"));
 	console.log('Using INFURA**************************');
   }
 //ABI:
-const abi = web3.eth.contract([{"constant":true,"inputs":[],"name":"longitud_medidas","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_hash_medidor","type":"string"},{"name":"_energia","type":"uint256"},{"name":"_tiempo","type":"string"}],"name":"nueva_medida","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"pk2medidas","outputs":[{"name":"ID","type":"uint256"},{"name":"hash_medidor","type":"string"},{"name":"energia","type":"uint256"},{"name":"tiempo","type":"string"},{"name":"pk_medidor","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"pk2cuenta_medidas","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"medidas","outputs":[{"name":"ID","type":"uint256"},{"name":"hash_medidor","type":"string"},{"name":"energia","type":"uint256"},{"name":"tiempo","type":"string"},{"name":"pk_medidor","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}])
-const contractAddress = '0xe7357d40168d23b15e7c5516b5304680aac5a358'
+abi_path = require('./abi.json')
+const abi = web3.eth.contract(abi_path)
+const contractAddress = '0xC6f64C8eB20d01a2A16feC69A29170B9eED65905'
 const contract = abi.at(contractAddress)
 
 var data = []
@@ -42,11 +43,38 @@ express()
     res.json({data: data})
     data = []
 })
-.get('/getdata/:id',cors(), (req, res) => {
-    let id = req.params.id
-    contract.medidas(id,(err,dataout) => {
-        res.json({data: dataout})   
-     });  
+.get('/getdata/:pk',cors(), (req, res) => {
+    let pk = req.params.pk
+    contract.longitud_medidas((err,resp) => {
+        //console.log("Longitud del vector: " + resp.c[0])
+        for (i = 0 ; i < resp.c[0]; i++) {              
+          contract.medidas(i,(err,dataout) => {
+            if(String(dataout[4].toUpperCase()) == String(pk).toUpperCase()){
+              data.push(dataout) 
+            }else{
+              dataout = "pk no encontrada"
+              data.push(dataout) 
+            }
+          });  
+        }
+    }) 
+    res.json({data: data})
+    data = []
+})
+.get('/getlast/:pk',cors(), (req, res) => {
+  let pk = req.params.pk
+  contract.longitud_medidas((err,resp) => {       
+        contract.medidas(resp.c[0]-1,(err,dataout) => {
+          if(String(dataout[4].toUpperCase()) == String(pk).toUpperCase()){
+            data.push(dataout) 
+          }else{
+            dataout = "pk no encontrada"
+            data.push(dataout) 
+          }
+        });  
+  }) 
+  res.json({data: data})
+  data = []
 })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
