3e5cda2e78193248cdcb0cb83cbe05020535a164
edcalderon <edwardca12@gmail.com>
2019-08-09 15:02:58 -0500

    make it better

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ const PORT = process.env.PORT || 5000;
 const Web3 = require('web3') 
 const cors = require('cors')
 
-//Prueba Actualizar Archivo HOSTINGER
-if (typeof web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider
-	web3 = new Web3(web3.currentProvider);
-	console.log('Using MetaMask**************************');
-  } else {
-    console.log('No web3? You should consider trying MetaMask!')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-	web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/162c6026989446e08fb54b3fe3888f12"));
-	console.log('Using INFURA**************************');
-  }
-//ABI:
-abi_path = require('./abi.json')
-const abi = web3.eth.contract(abi_path)
-const contractAddress = '0xC6f64C8eB20d01a2A16feC69A29170B9eED65905'
-const contract = abi.at(contractAddress)
-
-var data = []
-var sorted_data = []
-
 express()
 .use(cors())
 .use(bodyParser.json())
@@ -33,56 +13,7 @@ express()
     res.render('index', {});
 })
 .get('/getdata', cors(), (req, res) => {
-    contract.longitud_medidas((err,resp) => {
-        //console.log("Longitud del vector: " + resp.c[0])  
-        for (i = 0 ; i < resp.c[0]; i++) {   
-          contract.medidas(i,(err,dataout) => {
-                data.push(dataout)           
-          });  
-        }
 
-    })  
-    sorted_data = data.sort(function(a, b) {
-      return (a[0].c) - (b[0].c);
-    });
-    res.json({data: sorted_data})
-    data = []
-})
-.get('/getdata/:pk',cors(), (req, res) => {
-    let pk = req.params.pk
-    contract.longitud_medidas((err,resp) => {
-        //console.log("Longitud del vector: " + resp.c[0])
-        for (i = 0 ; i < resp.c[0]; i++) {              
-          contract.medidas(i,(err,dataout) => {
-            if(String(dataout[4].toUpperCase()) == String(pk).toUpperCase()){
-              data.push(dataout) 
-            }else{
-              dataout = "pk no encontrada"
-              data.push(dataout) 
-            }
-          });  
-        }
-    }) 
-    sorted_data = data.sort(function(a, b) {
-      return (a[0].c) - (b[0].c);
-    });
-    res.json({data: sorted_data})
-    data = []
-})
-.get('/getlast/:pk',cors(), (req, res) => {
-  let pk = req.params.pk
-  contract.longitud_medidas((err,resp) => {       
-        contract.medidas(resp.c[0]-1,(err,dataout) => {
-          if(String(dataout[4].toUpperCase()) == String(pk).toUpperCase()){
-            data.push(dataout) 
-          }else{
-            dataout = "pk no encontrada"
-            data.push(dataout) 
-          }
-        });  
-  }) 
-  res.json({data: data})
-  data = []
 })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
